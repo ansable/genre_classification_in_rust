64276662a07fd82580f95714f5f04682b6004598
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2018-08-22 00:46:50 +0300

    Make master follow old version for now
    We can change our minds later, current master didn't work at all

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,39 @@ extern crate scanlex;
 extern crate select;
 extern crate stopwords;
 
-#[macro_use]
-extern crate lazy_static;
+extern crate time;
+use time::PreciseTime;
 
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
 mod preprocessing;
 use preprocessing::preprocess_file;
 use preprocessing::tokenize_text;
 
-// function to build empty hashmap with all words that appear in vocabulary
-// fn build_vocab(train_directory: &str) -> HashMap<std::string::String, usize> {
-//     let train_dir = fs::read_dir("./train").unwrap();
+// function to build empty vec with all words that appear in vocabulary
+fn build_vocab(train_directory: &str) -> (Vec<Vec<std::string::String>>, Vec<std::string::String>) {
+    let train_dir = fs::read_dir("./train").unwrap();
 
-//     let mut vocab = HashMap::new();
-//     for train_file in train_dir {
-//         for token in preprocess_file(false, train_file.unwrap().path().to_str().unwrap()) {
-//             if !vocab.contains_key(&token) {
-//                 vocab.insert(token, 0);
-//             }
-//         }
-//         println!("{:?}", vocab);
-//     }
-//     vocab
-// }
+    let mut vocab: Vec<std::string::String> = vec![];
+    let mut all_files: Vec<Vec<std::string::String>> = vec![];
+
+    let mut count = 0usize;
+
+    for train_file in train_dir {
+        count += 1;
+        println!("{:?}", count);
+        let tokens_for_file = &preprocess_file(false, train_file.unwrap().path().to_str().unwrap());
+        all_files.push(tokens_for_file.to_vec());
+
+        for token in tokens_for_file {
+            if !vocab.contains(&token) {
+                vocab.push(token.to_string());
+            }
+        }
+    }
+    (all_files, vocab)
+}
 
 fn main() {
     // type_of_the_function
@@ -38,15 +46,16 @@ fn main() {
     // tokenize text
     // assert_eq!(tokenize_text("??? who are! (CAT)))"),&["who", "are", "CAT"]);
 
-    let mut vocab = vec![];
+    // let lion = &preprocess_file(false, "train/ghost_of_alive.txt");
+    // println!("{:?}", lion);
 
-    let lion = &preprocess_file(false, "train/ghost_of_alive.txt", vocab);
-    println!("{:?}", lion);
+    let baby = &preprocess_file(false, "train/disappearence.txt");
+    println!("{:?}", baby);
+    // let start = PreciseTime::now();
 
-    // let baby = &preprocess_file(false, "train/A Final Warning.html", vocab);
-    // println!("{:?}", baby);
+    // let vocab = build_vocab("./train").1;
 
-    // let vocab = build_vocab("./train");
-
-    // println!("{:?}", vocab);
+    // let end = PreciseTime::now();
+    // println!("This program took {} seconds to run", start.to(end)); // current benchmark: 148s
+    // println!("{:?}", vocab.len());
 }
