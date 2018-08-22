96794262ebb7848d832a7ba3d4ecb529ad077a6a
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2018-08-22 10:52:43 +0300

    Word counts

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,20 @@ fn build_vocab(train_directory: &str) -> (Vec<Vec<std::string::String>>, Vec<std
     (all_files, vocab)
 }
 
+fn get_counts(all_files: &Vec<Vec<std::string::String>>, vocab: Vec<std::string::String>) -> Vec<Vec<usize>> {
+    let mut all_counts: Vec<Vec<usize>> = vec![];
+
+    for file in all_files {
+        let mut counts = vec![0; vocab.len()];
+
+        for token in file {
+            counts[vocab.iter().position(|ref t| t == &token).unwrap()] += 1;
+        }
+        all_counts.push(counts);
+    }
+    all_counts
+}
+
 fn main() {
     // type_of_the_function
     // assert_eq!(type_of_file("james.txt"), "txt");
@@ -46,11 +60,17 @@ fn main() {
     // tokenize text
     // assert_eq!(tokenize_text("??? who are! (CAT)))"),&["who", "are", "CAT"]);
 
-    // let lion = &preprocess_file(false, "train/ghost_of_alive.txt");
-    // println!("{:?}", lion);
+    // let baby = &preprocess_file(false, "train/disappearence.txt");
+    // println!("{:?}", baby);
 
-    let baby = &preprocess_file(false, "train/disappearence.txt");
-    println!("{:?}", baby);
+    let tuple = build_vocab("./train");
+
+    let all_files = tuple.0;
+    let vocab = tuple.1;
+
+    let all_counts = get_counts(&all_files, vocab);
+
+    println!("{:?}", all_counts);
     // let start = PreciseTime::now();
 
     // let vocab = build_vocab("./train").1;
