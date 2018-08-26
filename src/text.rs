37fbec869d94516acb96c0ba4f3658b1d67e0101
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2018-08-26 10:03:24 +0200

    No longer work with cumbersome Text struct

--- src/text.rs
+++ src/text.rs
@@ -3,13 +3,7 @@ use std;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Clone, Debug)]
-pub struct Text {
-    pub filename: std::string::String,
-    pub label: std::string::String,
-}
-
-pub fn read_filenames_and_labels(train_labels_file: &str) -> Vec<Text> {
+pub fn read_filenames_and_labels(train_labels_file: &str) -> Vec<(std::string::String, std::string::String)> {
     let train_labels_file = File::open(train_labels_file).expect("File not found");
     let buf = BufReader::new(train_labels_file);
 
@@ -17,7 +11,7 @@ pub fn read_filenames_and_labels(train_labels_file: &str) -> Vec<Text> {
         .map(|l| l.expect("Line could not be processed"))
         .collect();
 
-    let mut filenames_and_labels: Vec<Text> = vec![];
+    let mut filenames_and_labels: Vec<(std::string::String, std::string::String)> = vec![];
 
     for line in lines {
         let line_as_vec: Vec<std::string::String> =
@@ -25,10 +19,7 @@ pub fn read_filenames_and_labels(train_labels_file: &str) -> Vec<Text> {
         let filename = &line_as_vec[0];
         let label = &line_as_vec[1];
 
-        filenames_and_labels.push(Text {
-            filename: filename.to_string(),
-            label: label.to_string(),
-        })
+        filenames_and_labels.push((filename.to_string(), label.to_string()));
     }
     filenames_and_labels
 }
