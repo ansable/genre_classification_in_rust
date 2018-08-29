4b09b41348c7a71ed03489a8ba0ac9cc0eb9ae67
markolozajic <marko.lozajic@student.uni-tuebingen.de>
2018-08-29 22:55:54 +0200

    Create separate module for evaluation metrics

---
+++ src/evaluation.rs
@@ -0,0 +1,94 @@
+use std;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+// Precision: out of the times a label was predicted, {return values} of the time the system was in fact correct
+fn precision(
+    gold: &Vec<std::string::String>,
+    pred: &Vec<std::string::String>,
+    label: std::string::String,
+) -> f64 {
+    assert_eq!(
+        gold.len(),
+        pred.len(),
+        "Label vectors must have the same length"
+    );
+
+    let mut total_predicted_for_label = 0;
+    let mut true_positive_for_label = 0;
+
+    if !pred.contains(&label) {
+        return 0.0;
+    }
+
+    for i in 0..pred.len() {
+        if pred[i] == label {
+            total_predicted_for_label += 1;
+            if gold[i] == label {
+                true_positive_for_label += 1;
+            }
+        }
+    }
+    true_positive_for_label as f64 / total_predicted_for_label as f64
+}
+
+// Recall: out of the times a label should have been predicted, {return value} of the labels were correctly predicted
+fn recall(
+    gold: &Vec<std::string::String>,
+    pred: &Vec<std::string::String>,
+    label: std::string::String,
+) -> f64 {
+    let mut total_gold_for_label = 0;
+    let mut true_positive_for_label = 0;
+
+    if !gold.contains(&label) {
+        return 0.0;
+    }
+
+    for i in 0..gold.len() {
+        if gold[i] == label {
+            total_gold_for_label += 1;
+            if pred[i] == label {
+                true_positive_for_label += 1;
+            }
+        }
+    }
+    true_positive_for_label as f64 / total_gold_for_label as f64
+}
+
+// F1: harmonic average of precision and recall
+fn f1_score(precision: f64, recall: f64) -> f64 {
+    if precision + recall == 0.0 {
+        return 0.0;
+    }
+
+    2.0 * precision * recall / (precision + recall)
+}
+
+// Return macro-averaged precision, recall and F1 score for given predictions
+pub fn macro_averaged_evaluation(
+    gold: Vec<std::string::String>,
+    pred: Vec<std::string::String>,
+) -> (f64, f64, f64) {
+    let mut macro_averaged_precision = 0.0;
+    let mut macro_averaged_recall = 0.0;
+    let mut macro_averaged_f1_score = 0.0;
+
+    // the following line assumes every label appears at least once in the training data
+    let labels_set: HashSet<std::string::String> = HashSet::from_iter(gold.iter().cloned());
+
+    for label in labels_set.iter() {
+        macro_averaged_precision += precision(&gold, &pred, label.to_string());
+        macro_averaged_recall += recall(&gold, &pred, label.to_string());
+    }
+
+    macro_averaged_precision = macro_averaged_precision / labels_set.len() as f64;
+    macro_averaged_recall = macro_averaged_recall / labels_set.len() as f64;
+    macro_averaged_f1_score = f1_score(macro_averaged_precision, macro_averaged_recall);
+
+    (
+        macro_averaged_precision,
+        macro_averaged_recall,
+        macro_averaged_f1_score,
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,6 @@ use std::fs;
 use std::fs::File;
 use std::io::Read;
 
-use std::collections::HashSet;
-use std::iter::FromIterator;
-
 mod args;
 use args::parse_args;
 
@@ -40,6 +37,9 @@ use preprocessing::preprocess_file;
 
 mod text;
 use text::read_filenames_and_labels;
+
+mod evaluation;
+use evaluation::macro_averaged_evaluation;
 
 // function to get tokens from the whole training corpus
 fn get_tokens_and_counts_from_corpus(
@@ -105,10 +105,9 @@ fn get_tfdif_vectors(files: Vec<Vec<(std::string::String, usize)>>) -> Vec<Vec<f
     let vocab = VOCAB.lock().unwrap().to_vec();
 
     for (i, word) in vocab.iter().enumerate() {
-        println!("{}{:?}{}{}", "Idf section: ", i + 1, " / ", vocab.len());
         let mut word_in_document_count = 0;
         for doc in files.iter() {
-            match doc.binary_search_by_key(&word, |&(ref w,c)| w) {
+            match doc.binary_search_by_key(&word, |&(ref w, c)| w) {
                 Ok(w) => word_in_document_count += 1,
                 Err(w) => continue,
             }
@@ -122,7 +121,6 @@ fn get_tfdif_vectors(files: Vec<Vec<(std::string::String, usize)>>) -> Vec<Vec<f
     }
 
     for (i, doc) in files.iter().enumerate() {
-        println!("{}{:?}{}{}", "Tf section: ", i + 1, " / ", files.len());
         let mut tfidf_vector: Vec<f64> = vec![0f64; vocab.len()];
 
         let mut total_words = 0;
@@ -200,15 +198,15 @@ fn matrix_to_vec(matrix: Vec<Vec<f64>>) -> (usize, usize, Vec<f64>) {
 }
 
 //SVD (using la crate)
-fn perform_svd(matrix: Vec<Vec<f64>>) -> (SVD<f64>, Matrix1<f64>){
-    let (n_rows,n_cols,data) = matrix_to_vec(matrix);
-    let mut la_matrix = Matrix1::new(n_rows,n_cols,data);
+fn perform_svd(matrix: Vec<Vec<f64>>) -> (SVD<f64>, Matrix1<f64>) {
+    let (n_rows, n_cols, data) = matrix_to_vec(matrix);
+    let mut la_matrix = Matrix1::new(n_rows, n_cols, data);
     let svd = SVD::new(&la_matrix);
-    return (svd,la_matrix);
+    return (svd, la_matrix);
 }
 
 //fixed
-fn svd_to_matrix(double: (SVD<f64>,Matrix1<f64>)) -> Matrix1<f64> {
+fn svd_to_matrix(double: (SVD<f64>, Matrix1<f64>)) -> Matrix1<f64> {
     let (svd, matrix) = double;
 
     let s = svd.get_s();
@@ -227,7 +225,7 @@ fn svd_to_matrix(double: (SVD<f64>,Matrix1<f64>)) -> Matrix1<f64> {
     let zeros = zeros.mt();
     let mut u = svd.get_u();
 
-    let result = s.mmul(u,zeros);
+    let result = s.mmul(u, zeros);
     result.clone()
 }
 // this one doesn't seem to be working at all, but we can give it another chance on a larger data set
@@ -270,90 +268,6 @@ fn save_pred_labels_to_vec(matrix: Matrix<f64>) -> Vec<std::string::String> {
     preds
 }
 
-// Precision: out of the times a label was predicted, {return values} of the time the system was in fact correct
-fn precision(
-    gold: &Vec<std::string::String>,
-    pred: &Vec<std::string::String>,
-    label: std::string::String,
-) -> f64 {
-    assert_eq!(
-        gold.len(),
-        pred.len(),
-        "Label vectors must have the same length"
-    );
-
-    let mut total_predicted_for_label = 0;
-    let mut true_positive_for_label = 0;
-
-    if !pred.contains(&label) {
-        return 0.0;
-    }
-
-    for i in 0..pred.len() {
-        if pred[i] == label {
-            total_predicted_for_label += 1;
-            if gold[i] == label {
-                true_positive_for_label += 1;
-            }
-        }
-    }
-    true_positive_for_label as f64 / total_predicted_for_label as f64
-}
-
-// Recall: out of the times a label should have been predicted, {return value} of the labels were correctly predicted
-fn recall(
-    gold: &Vec<std::string::String>,
-    pred: &Vec<std::string::String>,
-    label: std::string::String,
-) -> f64 {
-    let mut total_gold_for_label = 0;
-    let mut true_positive_for_label = 0;
-
-    if !gold.contains(&label) {
-        return 0.0;
-    }
-
-    for i in 0..gold.len() {
-        if gold[i] == label {
-            total_gold_for_label += 1;
-            if pred[i] == label {
-                true_positive_for_label += 1;
-            }
-        }
-    }
-    true_positive_for_label as f64 / total_gold_for_label as f64
-}
-
-// F1: harmonic average of precision and recall
-fn f1_score(precision: f64, recall: f64) -> f64 {
-    if precision + recall == 0.0 {
-        return 0.0;
-    }
-
-    2.0 * precision * recall / (precision + recall)
-}
-
-fn macro_averaged_evaluation(gold: Vec<std::string::String>, pred: Vec<std::string::String>) -> (f64, f64, f64) {
-    let mut macro_averaged_precision = 0.0;
-    let mut macro_averaged_recall = 0.0;
-    let mut macro_averaged_f1_score = 0.0;
-
-    // the following line assumes every label appears at least once in the training data
-    let labels_set: HashSet<std::string::String> = HashSet::from_iter(gold.iter().cloned());
-
-    for label in labels_set.iter() {
-        macro_averaged_precision += precision(&gold, &pred, label.to_string());
-        macro_averaged_recall += recall(&gold, &pred, label.to_string());
-    }
-
-    macro_averaged_precision = macro_averaged_precision / labels_set.len() as f64;
-    macro_averaged_recall = macro_averaged_recall / labels_set.len() as f64;
-    macro_averaged_f1_score = f1_score(macro_averaged_precision, macro_averaged_recall);
-
-
-    (macro_averaged_precision, macro_averaged_recall, macro_averaged_f1_score)
-}
-
 fn main() {
     let start = PreciseTime::now();
 
@@ -377,7 +291,6 @@ fn main() {
     );
 
     let tfidf_matrix_train = get_tfdif_vectors(train_files_and_counts);
-    println!("{:?}", tfidf_matrix_train[0]);
 
     // let (test_files_and_counts, labels_test) = get_tokens_and_counts_from_corpus(
     //     test_dir,
@@ -385,12 +298,6 @@ fn main() {
     //     matches.is_present("stopwords"),
     //     false,
     // );
-
-    
-
-
-
-
 
     // println!("{:?}", "Loading training document matrix...");
     // let tfidf_matrix_train = read_matrix_from_compressed_file("models/matrix_train.pickle.zip");
